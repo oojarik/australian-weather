1c43cee48d1b506eaef29c8136ac026e26861c79
Andrew Ojarikre <oojarik@live.com>
2021-02-24 14:28:02 +0000

    added time series

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,15 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(colour = "red")
+  geom_point(colour = "red") + theme_minimal()
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
+
+time_series =
+  df %>%
+  filter(Location == city) %>%
+  ggplot(aes(x = Date, y = Temp9am)) +
+  geom_line()
+ggsave(filename = "graphics/timeseries-rainfall.png",
+       plot = scatter_plot)
